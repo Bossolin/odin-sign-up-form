6953ae318393098f71e71d7295976258cee17090
Bossolin <vladyslav.okipnyak@icloud.com>
2022-03-06 01:12:01 +0100

    add style to picture div

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import background from "./img/aurora.JPG";
 
 function App() {
   return (
-    <div className="">
-      <div style={{ backgroundImage: `url(${background})` }}>
-        <div>
-          <img src={logo} alt="Odin logo" />
-          <span className="font-odin">ODIN</span>
+    <div className="sm:flex h-screen">
+      <div
+        className=" sm:w-2/5 min-w-fit text-center"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+        <div className="flex justify-center items-center my-4 text-white bg-opacity-40 bg-black">
+          <img src={logo} alt="Odin logo" className="w-24 p-2" />
+          <div className="font-odin text-8xl pt-4 ">ODIN</div>
         </div>
-        <p>Photo by Mattis Knudsen</p>
+        <p className="">Photo by Mattis Knudsen</p>
       </div>
-      <div>
+      <div className="p-4 ">
         <p>
           This is not a real online service! You know you need something like
           this in your life to help you realize your deepest dreams. Sign up{" "}
