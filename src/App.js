db420055dfac62c9d4ad24c9c158caab3bb35d8b
Bossolin <vladyslav.okipnyak@icloud.com>
2022-03-07 00:00:36 +0100

    fix image div

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ function App() {
   return (
     <div className="sm:flex h-screen">
       <div
-        className=" sm:w-2/5 min-w-fit text-center"
+        className=" sm:w-5/12 min-w-fit text-center bg-cover bg-center sm:min-h-screen sm:flex sm:flex-col sm:justify-between"
         style={{ backgroundImage: `url(${background})` }}
       >
-        <div className="flex justify-center items-center my-4 text-white bg-opacity-40 bg-black">
+        <div className="flex justify-center items-center p-2 sm:mt-56 text-white bg-opacity-40 bg-black">
           <img src={logo} alt="Odin logo" className="w-24 p-2" />
           <div className="font-odin text-8xl pt-4 ">ODIN</div>
         </div>
-        <p className="">Photo by Mattis Knudsen</p>
+        <p className="text-gray-400 bg-black bg-opacity-40 sm:bg-opacity-0 hidden sm:block">
+          Photo by Mattis Knudsen
+        </p>
       </div>
-      <div className="p-4 ">
+      <div className="p-4 bg-slate-100 shadow-inner ">
         <p>
           This is not a real online service! You know you need something like
           this in your life to help you realize your deepest dreams. Sign up{" "}
